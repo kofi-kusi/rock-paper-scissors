3f3184121ee923176c7a34346744ff389ee761bd
kusi <appaukusi05@gmail.com>
2024-05-20 14:25:35 +0000

    added getComputerChoice function the returns computers choice.

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function getComputerChoice(){
+    let computerChoice = Math.floor(Math.random()*(3-1+1)) + 1;
+    if (computerChoice === 1){
+        return "rock";
+    }else if (computerChoice === 2){
+        return "paper";
+    }else{
+        return "scissors";
+    }
+}
+
+
+
